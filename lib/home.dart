4ab70399eadb0df0905230efda5c93119943aa77
HAMOUDA\Hamouda <mohamed.hamouda7897@gmail.com>
2024-02-24 12:45:53 +0200

    add bottom nav bar and floating action button

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:todo_c10_str/tabs/settings_tab.dart';
+import 'package:todo_c10_str/tabs/tasks_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = "HomeScreen";
 
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
+      backgroundColor: Color(0xFFDFECDB),
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text(
@@ -18,6 +29,45 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8,
+        height: 60,
+        padding: EdgeInsets.zero,
+        child: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (value) {
+            index = value;
+            setState(() {});
+          },
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.task), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.white, width: 3),
+            borderRadius: BorderRadius.circular(30)),
+        child: Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+      body: tabs[index],
     );
   }
+
+  List<Widget> tabs = [TasksTab(), SettingsTab()];
 }
